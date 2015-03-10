fe3f7ce86ed01d9fadbde5c44251713c214f382c
unknown <jinhoj@JINHOJ-X220.redmond.corp.microsoft.com>
2015-03-10 15:30:17 -0700

    Update the path to point to website/index.html

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ var http = require("http"),
   fs = require("fs");
 
 http.createServer(function(request, response) {
-  fs.readFile("./index.html", "binary", function(err, file) {
+  fs.readFile("./../website/index.html", function(err, file) {
     if (err) {
       response.writeHead(500, {"Content-type" : "text/plain"});
       response.end(err + "\n");
       return;
     }
-    response.writeHead(200, {"Content-type" : "text/plain"});
-    response.end(file, "binary");
+    response.writeHead(200, {"Content-type" : "text/html"});
+    response.write(file);
+    response.end();
   });
 }).listen(8080);
