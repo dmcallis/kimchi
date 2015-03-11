741994f9929e11650fa46ca17c74527b6f0e76a4
Jin Ho Jeon <jinhoj@microsoft.com>
2015-03-11 13:30:51 -0700

    Update http://localhost:8080/boards/:id REST API

--- server/kimchi.js
+++ server/kimchi.js
@@ -1,25 +1,54 @@
-var fs = require('fs')
+var fs = require('fs');
+
+function contains(key, value) {
+    return true;
+}
+
+try {
+    var data = fs.readFileSync("../website/samplejson/board.json", "UTF-8");
+    var boards = JSON.parse(data);
+}
+catch (e) {
+    console.log(e);
+}
 
 exports.boards = function (request, response) {
-    try {
-        var data = fs.readFileSync("../website/samplejson/board.json", "UTF-8")
-        response.send(data)
-    }
-    catch(e) { console.log(e) }
+    response.send(JSON.stringify(boards));
 };
  
-exports.getBoard = function(request, response) {
-    response.send("Get Board: " + request.params.id)
+exports.getBoard = function (request, response) {
+    for (var i = 0; i < boards.length; i++) {
+        if (boards[i].Id == request.params.id) {
+            response.send(JSON.stringify(boards[i]));
+            return;
+        }
+    }
+
+    response.status(400).send("Board (Id: " + request.params.id + ") not found");
 };
 
 exports.addBoard = function (request, response) {
-    response.send("Add Board: " + request.body)
+    response.status(200).send("Board created");
 };
 
 exports.updateBoard = function (request, response) {
-    response.send("Update Board: " + request.params.id)
+    for (var i = 0; i < boards.length; i++) {
+        if (boards[i].Id == request.params.id) {
+            response.send(JSON.stringify(boards[i]));
+            return;
+        }
+    }
+
+    response.status(400).send("Board (Id: " + request.params.id + ") not found");
 };
 
 exports.deleteBoard = function (request, response) {
-    response.send("Delete Board: " + request.params.id)
+    for (var i = 0; i < boards.length; i++) {
+        if (boards[i].Id == request.params.id) {
+            response.status(200).send("Board (Id: " + request.params.id + ") will be deleted by Jun Yong Kim in the near future. : )");
+            return;
+        }
+    }
+
+    response.status(400).send("Board (Id: " + request.params.id + ") not found");
 };
