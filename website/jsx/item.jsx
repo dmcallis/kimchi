4d168fd9a3abe28e5adb89d35719260ad80853f3
YongJin <yjdotkim@live.com>
2015-03-11 18:43:38 -0700

    Basic UI to edit list title

--- website/jsx/item.jsx
+++ website/jsx/item.jsx
@@ -32,17 +32,37 @@ var NewItemForm = React.createClass({
 });
 
 var Item = React.createClass({
-  render: function() {
-    var divId = this.props.ListId + "_" + this.props.Id; // TODO: Unique id?
-    return (
-      <div id = {divId} className="item">
-          <h3 className="itemContent">{ this.props.Content }</h3>
-          <h5 className="itemOwner">Owner { this.props.Owner }</h5>
-          <h5 className="itemCreatedDate">Created { this.props.CreatedDate }</h5>
-          <h5 className="itemCreatedDate">Modified { this.props.ModifiedDate }</h5>
-      </div>
-    );
-  }
+    render: function() {
+        // var displayBox = $("#itemContent_Display_" + this.props.ListId + "_" + this.props.Id);
+        // var editBox = $("#itemContent_Edit_" + this.props.ListId + "_" + this.props.Id);
+        // var editCancelButton = $("#itemContent_Edit_Cancel_" + this.props.ListId + "_" + this.props.Id);
+        //
+        // editBox.hide();
+        //
+        // displayBox.click(function() {
+        //     displayBox.hide("slow");
+        //     editBox.show("slow");
+        // });
+        //
+        // editCancelButton.click(function () {
+        //     editBox.hide("slow");
+        //     displayBox.show("slow");
+        // });
+
+        var divId = "item_" + this.props.ListId + "_" + this.props.Id; // TODO: Unique id?
+        return (
+          <div id = {divId} className="item">
+            <div id={ "itemContent_" + this.props.ListId + "_" + this.props.Id }>
+                <div id={ "itemContent_Display_" + this.props.ListId + "_" + this.props.Id }>
+                    <h3 className="itemContent">{ this.props.Content }</h3>
+                </div>
+            </div>
+            <h5 className="itemOwner">Owner { this.props.Owner }</h5>
+            <h5 className="itemCreatedDate">Created { this.props.CreatedDate }</h5>
+            <h5 className="itemCreatedDate">Modified { this.props.ModifiedDate }</h5>
+          </div>
+        );
+    }
 });
 
 var Items = React.createClass({
